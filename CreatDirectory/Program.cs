9c3579bc11f51a7a3a5eaa64f19d775b56d5fa46
GlobalEmanuel <global.emanuel@ao3tech.com>
2022-01-03 19:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ CreatDirectory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace CreatDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderName = "pasta";
+            var subFolderName = "subpasta";
+            var subFolderNameStatic = "subpasta_usingstatic";
+
+            var directoryInfoSubfolder = new DirectoryInfo(subFolderName);
+            if(!Directory.Exists(folderName) || !directoryInfoSubfolder.Exists)
+            {
+                Directory.CreateDirectory(folderName);
+                Directory.CreateDirectory(subFolderNameStatic);
+                directoryInfoSubfolder.Create();
+
+                directoryInfoSubfolder.MoveTo($"{folderName}//{subFolderName}");
+                Directory.Move(subFolderNameStatic, @$"{folderName}\{subFolderNameStatic}");
+            }
+
+            var name = directoryInfoSubfolder.Name;
+            var parent = directoryInfoSubfolder.Parent;
+            var root = directoryInfoSubfolder.Root;
+            var exists = directoryInfoSubfolder.Exists;
+
+            foreach (var directory in Directory.GetDirectories(folderName))
+            {
+                Console.WriteLine(directory);
+            }
+            Directory.Delete($@"{folderName}\{subFolderName}");
+        }
+    }
+}
